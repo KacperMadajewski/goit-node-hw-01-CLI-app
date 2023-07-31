444cff1554ad4d850e991480a67da45d404ba940
KacperMadajewski <kacpermadajewski@gmail.com>
2023-07-31 23:05:26 +0200

    step-2 not finished

---
+++ contacts.js
@@ -0,0 +1,22 @@
+import fs from "node:fs";
+import path from "node:path";
+
+
+const contactsPath = path.dirname('/db/contacts.json');
+
+
+function listContacts() {
+  
+}
+
+function getContactById(contactId) {
+  // ...twój kod
+}
+
+function removeContact(contactId) {
+  // ...twój kod
+}
+
+function addContact(name, email, phone) {
+  // ...twój kod
+}
