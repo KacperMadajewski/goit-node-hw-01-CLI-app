0ba252035005b624081451dd9cff0dbc13dc3015
KacperMadajewski <kacpermadajewski@gmail.com>
2023-08-03 14:51:31 +0200

    added functions and done step - 2

--- contacts.js
+++ contacts.js
@@ -1,23 +1,36 @@
 import fs from "node:fs";
 import path from "node:path";
 
-
-const contactsPath = path.dirname('/db/contacts.json');
-
+const contactsPath = path.dirname("/db/contacts.json");
 
 function listContacts() {
   const data = fs.readFile(contactsPath);
   const parsedData = JSON.parse(data);
+  return parsedData;
 }
 
 function getContactById(contactId) {
-  // ...twój kod
+  const contacts = listContacts();
+  const findContact = contacts.find((contact) => contact.id === contactId);
+  return findContact;
 }
 
 function removeContact(contactId) {
-  // ...twój kod
+  const contacts = listContacts();
+  const newContacts = contacts.filter((contact) => contact.id !== contactId);
+  return newContacts;
 }
 
 function addContact(name, email, phone) {
-  // ...twój kod
+  const contacts = listContacts();
+  const newContact = { id: Date.now().toString(), name, email, phone };
+  const newContacts = contacts.push(newContact);
+  return newContacts;
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
