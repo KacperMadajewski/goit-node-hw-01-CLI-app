73eee20a6b6a31fc316d1741c04ec3b8fe2a91a2
KacperMadajewski <kacpermadajewski@gmail.com>
2023-08-08 14:57:32 +0200

    done app

--- contacts.js
+++ contacts.js
@@ -1,31 +1,73 @@
-import fs from "node:fs";
-import path from "node:path";
+const fs = require("fs").promises;
+const path = require("path");
+require("colors");
 
-const contactsPath = path.dirname("/db/contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-function listContacts() {
-  const data = fs.readFile(contactsPath);
-  const parsedData = JSON.parse(data);
-  return parsedData;
+async function contactsJSON() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const parsedData = await JSON.parse(data);
+    return parsedData;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 
-function getContactById(contactId) {
-  const contacts = listContacts();
-  const findContact = contacts.find((contact) => contact.id === contactId);
-  return findContact;
+async function listContacts() {
+  const contacts = await contactsJSON();
+  console.table(contacts);
 }
 
-function removeContact(contactId) {
-  const contacts = listContacts();
-  const newContacts = contacts.filter((contact) => contact.id !== contactId);
-  return newContacts;
+async function getContactById(contactId) {
+  try {
+    const contacts = await contactsJSON();
+    const findContact = contacts.find((item) => item.id === contactId);
+
+    if (!findContact) {
+      console.log(
+        `Can't find contact with this id: ${contactId}, please try again.`.red
+      );
+      return;
+    }
+
+    console.table(findContact);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function addContact(name, email, phone) {
-  const contacts = listContacts();
-  const newContact = { id: Date.now().toString(), name, email, phone };
-  const newContacts = contacts.push(newContact);
-  return newContacts;
+async function removeContact(contactId) {
+  try {
+    const contacts = await contactsJSON();
+    const searchContact = contacts.findIndex((item) => item.id === contactId);
+
+    if (searchContact === -1) {
+      console.log(
+        `Can't find contact with this id: ${contactId}, please try again.`.red
+      );
+      return;
+    }
+
+    contacts.splice(searchContact, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    console.table(contacts);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await contactsJSON();
+    const newContact = { id: Date.now().toString(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    console.table(contacts);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-const contacts = require("./contacts");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./contacts.js");
 
-const argv = require("yargs").argv;
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-// TODO: refaktor
+program.parse(process.argv);
+
+const argv = program.opts();
+
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      listContacts();
       break;
 
     case "get":
-      // ... id
+      getContactById(id);
       break;
 
     case "add":
-      // ... name email phone
+      addContact(name, email, phone);
       break;
 
     case "remove":
-      // ... id
+      removeContact(id);
       break;
 
     default:
